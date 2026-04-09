e5ab23bd233662faa9a0ff1bf069087c84eedf53
Melvin Vermast <143947718+Smulvin@users.noreply.github.com>
2026-04-09 14:55:49 +0200

    detail styling

--- src/pages/shared.ts
+++ src/pages/shared.ts
@@ -1,1 +1,10 @@
-export const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon?limit=809" //809 voor tot melmetal
+export const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon?limit=898" 
+//Gen 1: 151
+//Gen 2: 251
+//Gen 3: 386
+//Gen 4: 493
+//Gen 5: 649
+//Gen 6: 721
+//Gen 7: 809
+//Gen 8: 898
+//Gen 9: 1010
